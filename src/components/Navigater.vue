5fb9592b66b018d3821d6d7aebc89c4afff02c78
Hubery Yang <78254474+Hub-yang@users.noreply.github.com>
2025-05-30 21:30:11 +0800

    feat: 优化navigater组件编写

--- src/components/Navigater.vue
+++ src/components/Navigater.vue
@@ -3,29 +3,19 @@ const links = [
   {
     title: 'Vue',
     url: '',
+    className: 'vue group',
   },
   {
     title: 'to React',
     url: 'https://huberyyang.site:86/',
+    className: 'react',
   },
   {
     title: 'to Html',
     url: 'https://huberyyang.site/',
+    className: 'html',
   },
 ]
-
-function getItemClass(key: string): string {
-  switch (key) {
-    case 'Vue':
-      return 'ml8px px12px py4px rounded-8px bg-#42b883 hover:bg-white color-black hover:color-#42b883  text-center relative transition-all transition-ease transition-duration-300'
-    case 'to React':
-      return 'ml12px transition-color transition-ease transition-300 hover:color-#087ea4 cursor-pointer'
-    case 'to Html':
-      return 'ml12px transition-color transition-ease transition-300 hover:color-#dd4b25 cursor-pointer'
-    default:
-      return ''
-  }
-}
 
 function onNavigate(url: string) {
   if (!url)
@@ -35,9 +25,9 @@ function onNavigate(url: string) {
 </script>
 
 <template>
-  <div flex="~" absolute left="10px" top="10px" z-1000 items-center text-xl font-bold>
+  <div flex="~" left="10px" top="10px" absolute z-1000 items-center text-18px font600>
     <span>Build with</span>
-    <span v-for="{ title, url } in links" :key="title" class="group" :class="getItemClass(title)" @click="onNavigate(url)">{{ title }}
+    <span v-for="{ title, url, className } in links" :key="title" :class="[className]" transition="all ease 600" @click="onNavigate(url)">{{ title }}
       <span v-if="title === 'Vue'" left="50%" absolute bottom-6px h2px w0px translate-x--20px group-hover:w40px bg="#42b883" transition="width ease 600" />
     </span>
   </div>
--- uno.config.ts
+++ uno.config.ts
@@ -9,6 +9,10 @@ import {
 export default defineConfig({
   shortcuts: [
     ['btn', 'rounded-2 border border-color-transparent px-2 py-1 cursor-pointer transition-duration-250 transition-property-border-color bg-#f9f9f9  color-black hover:border-color-#646cff inline-block'],
+    ['vue', 'ml8px px12px py4px rounded-8px bg-#42b883 hover:bg-white color-black hover:color-#42b883 text-center relative'],
+    ['react', 'ml12px hover:color-#087ea4 cursor-pointer'],
+    ['html', 'ml12px hover:color-#dd4b25 cursor-pointer'],
+
   ],
   presets: [
     presetUno(),
